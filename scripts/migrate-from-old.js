abdee70a7e0ba3ab109c94f4193c05128f3b9cb5
Perry Mitchell <perry@perrymitchell.net>
2021-07-08 22:36:42 +0300

    Setup old site migration

---
+++ scripts/migrate-from-old.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const fs = require("fs");
+const frontMatter = require("front-matter");
+const YAML = require("yaml");
+
+const POSTS_INPUT = "/Users/perry/temp/website/perrymitchell.net/posts";
+const POSTS_OUTPUT = "/Users/perry/git/perrymitchell.net/src/posts";
+
+async function run() {
+
+    const inputResults = fs
+        .readdirSync(POSTS_INPUT, {
+            withFileTypes: true
+        })
+        .map(dirent => ({
+            filename: path.resolve(POSTS_INPUT, dirent.name),
+            type: dirent.isFile() ? "file" : "directory"
+        }))
+        .filter(item => item.type === "file");
+
+    for (const sourceItem of inputResults) {
+        const md = fs.readFileSync(sourceItem.filename, "utf8");
+        const {
+            attributes,
+            body
+        } = frontMatter(`---\n${md}`);
+        const {
+            title,
+            tags = [],
+            date: dateRaw
+        } = attributes;
+        const postDate = new Date(dateRaw);
+        const [dateSlug] = postDate.toISOString().split("T");
+        const fileNameBase = path.basename(sourceItem.filename).replace(".md", "");
+
+        const excerpt = `${body.trim().substring(0, 100)}...`;
+
+        const newPostName = `${dateSlug}-${fileNameBase}`;
+        const newPostDir = path.join(POSTS_OUTPUT, newPostName);
+        fs.mkdirSync(newPostDir, { recursive: true });
+
+        const newFrontMatter = {
+            layout: "post",
+            title,
+            excerpt,
+            date: dateSlug,
+            updatedDate: dateSlug,
+            tags: ["post", ...(tags || [])]
+        };
+
+        fs.writeFileSync(
+            path.join(newPostDir, "index.md"),
+            `---\n${YAML.stringify(newFrontMatter).trim()}\n---\n\n${body}`
+        );
+
+        // Copy images
+        const imageDir = path.join(POSTS_INPUT, fileNameBase);
+        try {
+            fs.statSync(imageDir);
+        } catch (err) {
+            continue;
+        }
+
+        const images = fs.readdirSync(imageDir);
+        for (const image of images) {
+            const sourcePath = path.join(imageDir, image);
+            const destPath = path.join(newPostDir, image);
+            fs.copyFileSync(sourcePath, destPath);
+        }
+    }
+
+}
+
+run().catch(err => {
+    console.error(err);
+});
